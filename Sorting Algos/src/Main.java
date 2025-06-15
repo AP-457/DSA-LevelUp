46a3735690a024fa40416b4998c1b4a894c64b3e
AP-457 <adityapande457@gmail.com>
2025-06-15 10:52:01 +0530

    Created Module for Sorting Algos

---
+++ Sorting Algos/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        1. Selection Sort
+//        2. Bubble Sort
+//        3. Insertion Sort
+//        4. Merge Sort
+//        5. Quick Sort
+//        6. Recursive Bubble Sort
+//        7. Recursive Insertion Sort
+
+
+    }
+}
