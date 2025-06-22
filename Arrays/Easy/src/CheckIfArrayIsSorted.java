50959b6f37fba419fbd32d08fa5b98d2f2e1886c
AP-457 <adityapande457@gmail.com>
2025-06-22 21:24:19 +0530

    Solved question - Check if Array is sorted

---
+++ Arrays/Easy/src/CheckIfArrayIsSorted.java
@@ -0,0 +1,9 @@
+public class CheckIfArrayIsSorted {
+    public boolean check(int[] nums) {
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i]>nums[i-1]) return false;
+        }
+
+        return true;
+    }
+}
