0876c98634e2075da4ab7485dbe6d312d7cf2f46
AP-457 <adityapande457@gmail.com>
2025-09-04 00:06:42 +0530

    Implemented Lower Bound

---
+++ BinarySearch/Easy/LowerBound.java
@@ -0,0 +1,21 @@
+package BinarySearch.Easy;
+
+class LowerBound {
+    int lowerBound(int[] arr, int target) {
+        // code here
+        int si= 0, ei= arr.length-1;
+        int ans= arr.length;
+
+        while(si<=ei){
+            int mid= si+(ei-si)/2;
+            if(arr[mid]>=target){
+                ans= mid;
+                ei= mid-1;
+            }else{
+                si= mid+1;
+            }
+        }
+
+        return ans;
+    }
+}
